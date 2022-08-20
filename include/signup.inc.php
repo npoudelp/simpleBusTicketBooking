cbf9eb75c158e101ea7594a56e13f7551260dbdf
npoudelp <npoudelp@gmail.com>
2022-08-20 21:00:09 +0545

    some basic user validation is added

--- include/signup.inc.php
+++ include/signup.inc.php
@@ -1,21 +1,36 @@
 <?php
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
     $password = $_POST['password'];
     $passwordR = $_POST['passwordR'];
     $type = 'users';
-    if($password != $passwordR){
+    if ($password != $passwordR) {
         header("location: ../pages/signup.php?msg=failed");
     }
     include_once("./dbConn.inc.php");
-    $sql = "INSERT INTO users (name, phone, address, password, type) VALUES ('$name', '$phone', '$address', '$password', '$type');";
-    $res = mysqli_query($conn, $sql);
-    if($res){
-        header("location: ../pages/signup.php?msg=success");
-    }else{
-        header("location: ../pages/signup.php?msg=failed");
+
+    $checkUsers = "SELECT * FROM users WHERE phone='$phone';";
+    $check = mysqli_query($conn, $checkUsers);
+    if (mysqli_num_rows($check) > 0) {
+        while ($phone = mysqli_fetch_assoc($check)) {
+            if ($phone['phone'] == $phone) {
+                header("location: ../pages/signup.php?msg=phone_number_exists");
+                die();
+            }
+        }
     }
 
+    if ($password == $passwordR) {
+        $sql = "INSERT INTO users (name, phone, address, password, type) VALUES ('$name', '$phone', '$address', '$password', '$type');";
+        $res = mysqli_query($conn, $sql);
+        if ($res) {
+            header("location: ../pages/signup.php?msg=success");
+        } else {
+            header("location: ../pages/signup.php?msg=failed");
+        }
+    } else {
+        header("location: ../pages/signup.php?msg=password_confirm_not_matched");
+    }
 }
