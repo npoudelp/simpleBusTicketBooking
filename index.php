3d79faa50a517aea85ea6677b08602892d64dd45
npoudelp <npoudelp@gmail.com>
2022-08-20 21:01:18 +0545

    some details of counter added

--- index.php
+++ index.php
@@ -13,16 +13,16 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="./index.php">dTicket</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand text-success" href="./index.php">dTicket</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="./index.php">Home</a>
+                        <a class="nav-link active" href="./index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./pages/login.php"><span class="btn btn-success">Login</span></a>
@@ -31,6 +31,55 @@
             </div>
         </div>
     </nav>
+
+    <!-- Bus image here -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="./images/bus.jpg" class="figure-img img-fluid rounded" width="100%">
+                </div>
+                <div class="col-md-6">
+                    <p class="display-1 text-center text-success align-middle border-bottom">
+                        dTicket
+                    </p><br>
+                    <p class="h3 lead text-success diabled text-center">Your Ticket Booking Partner</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Bus image endes -->
+
+    <!-- Promotion starts here -->
+    <section class="lead p-5">
+        <div class="container">
+            <div class="row">
+                <div class="p-2 shadow bg-light col-md-4">
+                    <span class="h3 border-bottom">
+                        Who are we?
+                    </span><br>
+                    <span>We are the bus booking coounter based in Internet who work for the digital you.</span>
+                </div>
+
+                <div class="p-2 shadow bg-light col-md-4">
+                    <span class="h3 border-bottom">
+                        What we do?
+                    </span><br>
+                    <span>We provide the simpler and low effort bus ticket booking from your own phone.</span>
+                </div>
+
+                <div class="p-2 shadow bg-light col-md-4">
+                    <span class="h3 border-bottom">
+                        How to contact us?
+                    </span><br>
+                    <span>We do not have any physical address that you can visit for now. However you can contact us at 9812345678</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Promotion ends here -->
+
+
 
 </body>
 
